b152d1a129d06e16dd1128293347386abd271ca1
sarpt <mike.m.info@gmail.com>
2020-06-28 01:34:08 +0200

    Refactor and cleanup of the Document type - specific functions attached to OasObject as methods; OasObject holds an instance of it's underlying OpenAPI object along with its parent and information how it's stored in the parent for replacement

--- pkg/openapi/document.go
+++ pkg/openapi/document.go
@@ -36,7 +36,7 @@ type Document struct {
 
 // reference contains information about OpenAPI object that contains reference and path of reference
 type reference struct {
-	object oasObject
+	object OasObject
 	path   string
 }
 
@@ -136,12 +136,12 @@ func (doc *Document) SetRefDirectory(dir string) {
 // Currently when resolving references to the remote documents, their local references end up in the document being resolved.
 // This behavior ends up creating incorrect output file.
 func (doc Document) ResolveReferences() error {
-	rootObject := oasObject{
-		parent: &doc,
-		name:   RootItem,
+	rootObject, err := NewOasObjectByName(&doc, RootItem)
+	if err != nil {
+		return err
 	}
 
-	refs, err := doc.parseOASObject(rootObject)
+	refs, err := rootObject.references()
 	if err != nil {
 		return err
 	}
@@ -156,7 +156,7 @@ func (doc Document) ResolveReferences() error {
 			continue
 		}
 
-		err := doc.assignReference(ref)
+		err := doc.replaceReference(ref)
 		if err != nil {
 			return err
 		}
@@ -165,189 +165,23 @@ func (doc Document) ResolveReferences() error {
 	return nil
 }
 
-// parseOASObject takes an object and returns list of all references that need to be resolved for object to be independent from references objects
-func (doc Document) parseOASObject(object oasObject) ([]reference, error) {
-	var allRefs []reference
-
-	item, err := object.Get()
-	if err != nil {
-		return allRefs, err
-	}
-
-	switch item.Kind() {
-	case reflect.Ptr:
-		refPath, fields, err := parsePtrItem(item)
-		if err != nil {
-			return allRefs, err
-		}
-
-		if refPath != "" {
-			ref := reference{
-				object: object,
-				path:   refPath,
-			}
-			allRefs = append(allRefs, ref)
-		} else { // when refPath is in an oas object that is not a slice or map, other tahn $ref fields can be ignored per specification
-			for _, field := range fields {
-				obj := oasObject{
-					parent: item.Interface(),
-					name:   field,
-				}
-				objRefs, err := doc.parseOASObject(obj)
-				if err != nil {
-					return allRefs, err
-				}
-
-				allRefs = append(allRefs, objRefs...)
-			}
-		}
-	case reflect.Map:
-		refPaths, keys, err := parseMapItem(item)
-		if err != nil {
-			return allRefs, err
-		}
-
-		for _, refPath := range refPaths {
-			ref := reference{
-				object: object,
-				path:   refPath,
-			}
-			allRefs = append(allRefs, ref)
-		}
-
-		for _, key := range keys {
-			obj := oasObject{
-				parent: item.Interface(),
-				name:   key,
-			}
-			newRefs, err := doc.parseOASObject(obj)
-			if err != nil {
-				return allRefs, err
-			}
-			allRefs = append(allRefs, newRefs...)
-		}
-	case reflect.Slice:
-		refPaths, indexes, err := parseSliceItem(item)
-		if err != nil {
-			return allRefs, err
-		}
-
-		for _, refPath := range refPaths {
-			ref := reference{
-				object: object,
-				path:   refPath,
-			}
-			allRefs = append(allRefs, ref)
-		}
-
-		for _, idx := range indexes {
-			obj := oasObject{
-				parent: item.Interface(),
-				idx:    idx,
-			}
-			newRefs, err := doc.parseOASObject(obj)
-			if err != nil {
-				return allRefs, err
-			}
-			allRefs = append(allRefs, newRefs...)
-		}
-	default:
-		return allRefs, fmt.Errorf("could not parse field %s due to incorrect type", object.name)
-	}
-
-	return allRefs, nil
-}
-
-func parsePtrItem(item reflect.Value) (string, []string, error) {
-	var fieldsToParse []string
-
-	itemElem := item.Elem()
-	itemType := itemElem.Type()
-	for i := 0; i < itemElem.NumField(); i++ {
-		childItem := itemElem.Field(i)
-
-		if childItem.IsZero() {
-			continue
-		}
-
-		switch childItem.Kind() {
-		case reflect.String:
-			if itemType.Field(i).Name == Ref {
-				return childItem.String(), []string{}, nil // when ref found in an object then no need to parse other fields
-			}
-		case reflect.Struct, reflect.Ptr, reflect.Map, reflect.Slice, reflect.Array:
-			fieldsToParse = append(fieldsToParse, itemType.Field(i).Name)
-		}
-	}
-
-	return "", fieldsToParse, nil
-}
-
-func parseMapItem(item reflect.Value) ([]string, []string, error) {
-	var keysToParse []string
-	var refs []string
-
-	mapIter := item.MapRange()
-	for mapIter.Next() {
-		childItem := mapIter.Value()
-
-		if childItem.IsZero() {
-			continue
-		}
-
-		switch childItem.Kind() {
-		case reflect.String:
-			if mapIter.Key().String() == Ref {
-				refs = append(refs, childItem.String())
-			}
-		case reflect.Struct, reflect.Ptr, reflect.Map, reflect.Slice, reflect.Array:
-			keysToParse = append(keysToParse, mapIter.Key().String())
-		}
-	}
-
-	return refs, keysToParse, nil
-}
-
-func parseSliceItem(item reflect.Value) ([]string, []int, error) {
-	var refs []string
-	var indexesToParse []int
-
-	for i := 0; i < item.Len(); i++ {
-		childItem := item.Index(i)
-
-		if childItem.IsZero() {
-			continue
-		}
-
-		switch childItem.Kind() {
-		case reflect.String:
-			if childItem.String() == Ref {
-				refs = append(refs, childItem.String())
-			}
-		case reflect.Struct, reflect.Ptr, reflect.Map, reflect.Slice, reflect.Array:
-			indexesToParse = append(indexesToParse, i)
-		}
-	}
-
-	return refs, indexesToParse, nil
-}
-
-// assignReference replaces ref either with inline object or a local reference
-func (doc Document) assignReference(ref reference) error {
+// replaceReference replaces ref either with inline object or a local reference
+func (doc Document) replaceReference(ref reference) error {
 	referencedDocument, err := doc.getReferencedDocument(ref.path)
 	if err != nil {
 		return fmt.Errorf("could not get reference document: %w", err)
 	}
 
-	refItem, err := referencedDocument.getItemByPath(ref.path)
+	refItem, err := referencedDocument.getReferencedValueByPath(ref.path)
 	if err != nil {
 		return err
 	}
 
-	return replaceReference(ref.object, refItem)
+	return ref.object.Set(refItem)
 }
 
-func (doc Document) getItemByPath(refPath string) (interface{}, error) {
+// getReferencedValueByPath walks the provided reference path, trying obtain the oas object
+func (doc Document) getReferencedValueByPath(refPath string) (interface{}, error) {
 	itemNames := referencePathToItems(refPath)
 	var parentValue reflect.Value = reflect.ValueOf(doc.Root)
 
@@ -376,42 +210,6 @@ func (doc Document) getItemByPath(refPath string) (interface{}, error) {
 	return parentValue.Interface(), nil
 }
 
-func getFieldByTag(tag string, structItem reflect.Value) (reflect.Value, error) {
-	structItemType := structItem.Type()
-
-	for i := 0; i < structItemType.NumField(); i++ {
-		childField := structItemType.Field(i)
-		yamlKey := getYamlKeyFromField(childField)
-		if yamlKey == tag {
-			return structItem.Field(i), nil
-		}
-	}
-
-	return reflect.Value{}, fmt.Errorf("the field with tag %s could not be found in type %s", tag, structItemType.Name())
-}
-
-// replaceReference unsets the reference and inlines refered object
-func replaceReference(object oasObject, ref interface{}) error {
-	parentVal := reflect.ValueOf(object.parent)
-	refVal := reflect.ValueOf(ref)
-
-	switch parentVal.Kind() {
-	case reflect.Slice:
-		parentVal.Index(object.idx).Set(refVal)
-	case reflect.Ptr:
-		field := parentVal.Elem().FieldByName(object.name)
-		field.Set(refVal)
-	case reflect.Map:
-		key, _, err := itemFromMapByName(parentVal, object.name)
-		if err != nil {
-			return err
-		}
-
-		parentVal.SetMapIndex(key, refVal)
-	}
-	return nil
-}
-
 func (doc Document) getReferencedDocument(refPath string) (*Document, error) {
 	var referencedDocument *Document
 
@@ -435,6 +233,20 @@ func (doc Document) getReferencedDocument(refPath string) (*Document, error) {
 	}
 
 	return referencedDocument, nil
+}
+
+func getFieldByTag(tag string, structItem reflect.Value) (reflect.Value, error) {
+	structItemType := structItem.Type()
+
+	for i := 0; i < structItemType.NumField(); i++ {
+		childField := structItemType.Field(i)
+		yamlKey := getYamlKeyFromField(childField)
+		if yamlKey == tag {
+			return structItem.Field(i), nil
+		}
+	}
+
+	return reflect.Value{}, fmt.Errorf("the field with tag %s could not be found in type %s", tag, structItemType.Name())
 }
 
 func itemFromMapByName(mapVal reflect.Value, key string) (reflect.Value, reflect.Value, error) {
--- pkg/openapi/oas_object.go
+++ pkg/openapi/oas_object.go
@@ -5,27 +5,262 @@ import (
 	"reflect"
 )
 
-// oasObject respresent the object of the OpenAPI schema.
+var (
+	// ErrIncorrectParent occurs when instance of oasObject has parent that cannot be used to find the object.
+	ErrIncorrectParent = fmt.Errorf("parent has incorrect type")
+	// ErrIncorrectObjectType occurs when underlying instance of object has incorrect type and cannot be parsed.
+	ErrIncorrectObjectType = fmt.Errorf("object has incorrect type")
+)
+
+// OasObject respresent the object of the OpenAPI schema.
 // For a parent that is a pointer or a map, the name is used to hold the field name for which object is accessible in the parent.
 // For a parent that is a list, the index should be used to obtain the object from it.
-type oasObject struct {
-	parent interface{}
-	name   string
-	idx    int
+type OasObject struct {
+	parent   interface{}
+	instance interface{}
+	name     string
+	idx      int
 }
 
-// Get returns the reflect.Value of the OpenAPI object that is pointed by the instance.
-func (o oasObject) Get() (reflect.Value, error) {
+// NewOasObjectByName takes parent and a name under which object can be found
+func NewOasObjectByName(parent interface{}, name string) (OasObject, error) {
+	obj := OasObject{
+		parent: parent,
+		name:   name,
+	}
+
+	instance, err := obj.parse()
+	if err != nil {
+		return obj, err
+	}
+
+	obj.instance = instance
+
+	return obj, nil
+}
+
+// NewOasObjectByIdx takes parent and an index under which object can be found
+func NewOasObjectByIdx(parent interface{}, idx int) (OasObject, error) {
+	obj := OasObject{
+		parent: parent,
+		idx:    idx,
+	}
+
+	instance, err := obj.parse()
+	if err != nil {
+		return obj, err
+	}
+
+	obj.instance = instance
+
+	return obj, nil
+}
+
+// parse returns the interface of the OpenAPI object that is pointed by the instance.
+func (o OasObject) parse() (interface{}, error) {
 	parentVal := reflect.ValueOf(o.parent)
 	switch parentVal.Kind() {
 	case reflect.Ptr:
-		return parentVal.Elem().FieldByName(o.name), nil
+		return parentVal.Elem().FieldByName(o.name).Interface(), nil
 	case reflect.Map:
 		_, val, err := itemFromMapByName(parentVal, o.name)
-		return val, err
+		return val.Interface(), err
 	case reflect.Slice:
-		return parentVal.Index(o.idx), nil
+		return parentVal.Index(o.idx).Interface(), nil
 	default:
-		return reflect.Value{}, fmt.Errorf("provided parent for %s field is not a correct type", o.name)
+		return nil, ErrIncorrectParent
 	}
+}
+
+// Set unsets the underlying object and sets a provided value.
+func (o OasObject) Set(val interface{}) error {
+	parentVal := reflect.ValueOf(o.parent)
+	refVal := reflect.ValueOf(val)
+
+	switch parentVal.Kind() {
+	case reflect.Slice:
+		parentVal.Index(o.idx).Set(refVal)
+	case reflect.Ptr:
+		field := parentVal.Elem().FieldByName(o.name)
+		field.Set(refVal)
+	case reflect.Map:
+		key, _, err := itemFromMapByName(parentVal, o.name)
+		if err != nil {
+			return err
+		}
+
+		parentVal.SetMapIndex(key, refVal)
+	default:
+		return ErrIncorrectParent
+	}
+
+	return nil
+}
+
+// references returns list of all references that need to be resolved for object to be independent from its references.
+// That list includes children references along with object's own references since parsing is done recursively until refs in all possible descendants are found.
+func (o OasObject) references() ([]reference, error) {
+	var allRefs []reference
+
+	value := reflect.ValueOf(o.instance)
+
+	switch value.Kind() {
+	case reflect.Ptr:
+		refPath, fields, err := parsePtrValue(value)
+		if err != nil {
+			return allRefs, err
+		}
+
+		if refPath != "" {
+			ref := reference{
+				object: o,
+				path:   refPath,
+			}
+			allRefs = append(allRefs, ref)
+		} else { // when refPath is in an oas object that is not a slice or map, other tahn $ref fields can be ignored per specification
+			for _, field := range fields {
+				obj, err := NewOasObjectByName(value.Interface(), field)
+				if err != nil {
+					return allRefs, err
+				}
+
+				objRefs, err := obj.references()
+				if err != nil {
+					return allRefs, err
+				}
+
+				allRefs = append(allRefs, objRefs...)
+			}
+		}
+	case reflect.Map:
+		refPaths, keys, err := parseMapValue(value)
+		if err != nil {
+			return allRefs, err
+		}
+
+		for _, refPath := range refPaths {
+			ref := reference{
+				object: o,
+				path:   refPath,
+			}
+			allRefs = append(allRefs, ref)
+		}
+
+		for _, key := range keys {
+			obj, err := NewOasObjectByName(value.Interface(), key)
+			if err != nil {
+				return allRefs, err
+			}
+
+			newRefs, err := obj.references()
+			if err != nil {
+				return allRefs, err
+			}
+			allRefs = append(allRefs, newRefs...)
+		}
+	case reflect.Slice:
+		refPaths, indexes, err := parseSliceValue(value)
+		if err != nil {
+			return allRefs, err
+		}
+
+		for _, refPath := range refPaths {
+			ref := reference{
+				object: o,
+				path:   refPath,
+			}
+			allRefs = append(allRefs, ref)
+		}
+
+		for _, idx := range indexes {
+			obj, err := NewOasObjectByIdx(value.Interface(), idx)
+			if err != nil {
+				return allRefs, err
+			}
+
+			newRefs, err := obj.references()
+			if err != nil {
+				return allRefs, err
+			}
+			allRefs = append(allRefs, newRefs...)
+		}
+	default:
+		return allRefs, ErrIncorrectObjectType
+	}
+
+	return allRefs, nil
+}
+
+func parsePtrValue(value reflect.Value) (string, []string, error) {
+	var fieldsToParse []string
+
+	itemElem := value.Elem()
+	itemType := itemElem.Type()
+	for i := 0; i < itemElem.NumField(); i++ {
+		childItem := itemElem.Field(i)
+
+		if childItem.IsZero() {
+			continue
+		}
+
+		switch childItem.Kind() {
+		case reflect.String:
+			if itemType.Field(i).Name == Ref {
+				return childItem.String(), []string{}, nil // when ref found in an object then no need to parse other fields
+			}
+		case reflect.Struct, reflect.Ptr, reflect.Map, reflect.Slice, reflect.Array:
+			fieldsToParse = append(fieldsToParse, itemType.Field(i).Name)
+		}
+	}
+
+	return "", fieldsToParse, nil
+}
+
+func parseMapValue(value reflect.Value) ([]string, []string, error) {
+	var keysToParse []string
+	var refs []string
+
+	mapIter := value.MapRange()
+	for mapIter.Next() {
+		childItem := mapIter.Value()
+
+		if childItem.IsZero() {
+			continue
+		}
+
+		switch childItem.Kind() {
+		case reflect.String:
+			if mapIter.Key().String() == Ref {
+				refs = append(refs, childItem.String())
+			}
+		case reflect.Struct, reflect.Ptr, reflect.Map, reflect.Slice, reflect.Array:
+			keysToParse = append(keysToParse, mapIter.Key().String())
+		}
+	}
+
+	return refs, keysToParse, nil
+}
+
+func parseSliceValue(value reflect.Value) ([]string, []int, error) {
+	var refs []string
+	var indexesToParse []int
+
+	for i := 0; i < value.Len(); i++ {
+		childItem := value.Index(i)
+
+		if childItem.IsZero() {
+			continue
+		}
+
+		switch childItem.Kind() {
+		case reflect.String:
+			if childItem.String() == Ref {
+				refs = append(refs, childItem.String())
+			}
+		case reflect.Struct, reflect.Ptr, reflect.Map, reflect.Slice, reflect.Array:
+			indexesToParse = append(indexesToParse, i)
+		}
+	}
+
+	return refs, indexesToParse, nil
 }
